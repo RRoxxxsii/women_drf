1492d1eb2b752780a3ff26e62c8d37e991e8ec98
rroxxxsii <mishabur38@gmail.com>
2023-05-16 19:36:56 +0300

    models: 1) created Women model 2) created Category Model 3) made migrations
    admin:
    1) registered the models
    
    serializers:
    1) created a serializer for women model
    
    views:
    1) created a WomenAPIView with all CRUD operations available
    
    urls:
    1) created a list url for all objects
    2) created a detailed url

--- drf_women_site/women/views.py
+++ drf_women_site/women/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Women
+from .serializers import WomenSerializer
+
+
+class WomenAPIView(APIView):
+
+    def get(self, request):
+        w = Women.objects.all().values()
+        return Response({'posts': WomenSerializer(w, many=True).data})
+
+    def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({'post': serializer.data})
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({"error": "Method PUT not allowed"})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({"error": "Object does not exists"})
+
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"post": serializer.data})
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({"error": "Method DELETE not allowed"})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({"error": "Object does not exists"})
+
+        deleted_obj = instance.delete()
+
+        return Response({'Success': 'The object has been successfully deleted'})
+
