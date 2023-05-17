9e1886fdb9b9a9a33b9237ea6bec5e4d4a6e47c9
rroxxxsii <mishabur38@gmail.com>
2023-05-17 14:04:39 +0300

    serializers: 1) refactored serializer code. Changed serializer to inherit from ModelSerializer class instead of Serializer
    views:
    1) refactored views. Made the class-based view inherit from ListCreateAPIView
    
    urls:
    1) as the class name from views.py has been changed, amended the urls

--- drf_women_site/women/views.py
+++ drf_women_site/women/views.py
@@ -1,49 +1,15 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
 from .models import Women
 from .serializers import WomenSerializer
 
 
-class WomenAPIView(APIView):
+class WomenAPIList(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
 
-    def get(self, request):
-        w = Women.objects.all().values()
-        return Response({'posts': WomenSerializer(w, many=True).data})
 
-    def post(self, request):
-        serializer = WomenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
 
-        return Response({'post': serializer.data})
 
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get("pk", None)
-        if not pk:
-            return Response({"error": "Method PUT not allowed"})
 
-        try:
-            instance = Women.objects.get(pk=pk)
-        except:
-            return Response({"error": "Object does not exists"})
-
-        serializer = WomenSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"post": serializer.data})
-
-    def delete(self, request, *args, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({"error": "Method DELETE not allowed"})
-
-        try:
-            instance = Women.objects.get(pk=pk)
-        except:
-            return Response({"error": "Object does not exists"})
-
-        deleted_obj = instance.delete()
-
-        return Response({'Success': 'The object has been successfully deleted'})
 
