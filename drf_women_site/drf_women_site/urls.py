5a9b9f904ef9a2e1db11f5509365956a7718a442
rroxxxsii <mishabur38@gmail.com>
2023-05-18 08:07:05 +0300

    urls: 1) provided urls token authentication with djoser 2) provided url patterns for refresh and verify with djoser
    settings:
    1) configured settings for JWT authentication

--- drf_women_site/drf_women_site/urls.py
+++ drf_women_site/drf_women_site/urls.py
@@ -15,14 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from women import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('api/v1/women/', views.WomenAPIList.as_view()),
     path('api/v1/women/<int:pk>/', views.WomenAPIUpdate.as_view()),
     path('api/v1/womendelete/<int:pk>/', views.WomenAPIDestroy.as_view()),
+    path('api/v1/auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
