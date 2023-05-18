775db428a7ced9eb52ab8e00694d85c19f13d8b6
rroxxxsii <mishabur38@gmail.com>
2023-05-18 08:29:43 +0300

    settings: 1) configured settings for pagination
    views:
    1) created a pagination class for the list of women objects
    2) connected the class to class-based view WomenAPIList

--- drf_women_site/drf_women_site/settings.py
+++ drf_women_site/drf_women_site/settings.py
@@ -131,6 +131,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # REST FRAMEWORK
 
 REST_FRAMEWORK = {
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5,
+
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
